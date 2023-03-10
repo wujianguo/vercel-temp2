0fed94d838242007423af62cf8953233c3077399
wujianguo <lsjustin89@gmail.com>
2023-03-10 09:07:07 +0800

    feat: json response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello():  { [key: string]: string } {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World2!';
+  getHello(): { [key: string]: string } {
+    return { data: 'Hello World2!' };
   }
 }
